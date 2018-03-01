ea7c67771c3c25e1ff7f84c47780d9afcd030ad8
Andre Loureiro <loureiro.andrew@gmail.com>
2018-03-01 15:47:08 -0400

    refactor: Use the previous stateless components
    Signed-off-by: Andre Loureiro <loureiro.andrew@gmail.com>

--- src/components/BookShelves.js
+++ src/components/BookShelves.js
@@ -1,42 +1,22 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Link } from "react-router-dom";
-
-import Book from "./Book";
+import ShelfTitle from "./ShelfTitle";
+import ShelfButtonSearch from "./ShelfButtonSearch";
+import Shelves from "./Shelves";
 
 class BookShelves extends Component {
   static propTypes = {
     books: PropTypes.array.isRequired,
-    shelves: PropTypes.array.isRequired,
     onChangeBookShelf: PropTypes.func.isRequired
   };
 
   render() {
-    const { books, shelves, onChangeBookShelf } = this.props;
+    const { books, onChangeBookShelf } = this.props;
     return (
       <div className="list-books">
-        <div className="list-books-title">
-          <h1>MyReads</h1>
-        </div>
-
-        <div className="list-books-content">
-          {shelves.map(shelf => (
-            <div key={shelf.name} className="bookshelf">
-              <h2 className="bookshelf-title">{shelf.displayName}</h2>
-              <div className="bookshelf-books">
-                <ol className="books-grid">
-                  {books
-                    .filter(book => book.shelf === shelf.name)
-                    .map(book => <Book key={book.id} book={book} bookShelves={shelves} />)}
-                </ol>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
-        </div>
+        <ShelfTitle title="My Reads" />
+        <Shelves books={books} onChangeBookShelf={onChangeBookShelf} />
+        <ShelfButtonSearch label="Add Book" />
       </div>
     );
   }
